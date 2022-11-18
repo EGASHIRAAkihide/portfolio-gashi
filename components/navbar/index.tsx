d685b8fd42635d6dd5bf5673cd4d29cfde33142c
akihide.egashira <42921740+EGASHIRAAkihide@users.noreply.github.com>
2022-11-18 20:15:00 +0900

    feat: add NEXTUI, create header-nav component (#10)
    * (feat) [feature/issue-008] NEXTUIを導入, buttonでtest
    
    * (feat) [feature/issue-008] add navbar from NEXTUI

---
+++ components/navbar/index.tsx
@@ -0,0 +1,29 @@
+import { Navbar as NextUiNavbar, Button, Link, Text } from "@nextui-org/react";
+
+export default function Navbar() {  
+  return (
+    <NextUiNavbar isBordered variant="floating">
+      <NextUiNavbar.Brand>
+        <Text b color="inherit" hideIn="xs">
+          GASHI
+        </Text>
+      </NextUiNavbar.Brand>
+      <NextUiNavbar.Content hideIn="xs">
+        <NextUiNavbar.Link href="#">About</NextUiNavbar.Link>
+        <NextUiNavbar.Link isActive href="#">Works</NextUiNavbar.Link>
+        <NextUiNavbar.Link href="#">Contact</NextUiNavbar.Link>
+      </NextUiNavbar.Content>
+      {/* TODO: authentication付与 */}
+      {/* <NextUiNavbar.Content>
+        <NextUiNavbar.Link href="#">
+          Login
+        </NextUiNavbar.Link>
+        <NextUiNavbar.Item>
+          <Button auto flat as={Link} href="#">
+            Sign Up
+          </Button>
+        </NextUiNavbar.Item>
+      </NextUiNavbar.Content> */}
+    </NextUiNavbar>
+  )
+}
