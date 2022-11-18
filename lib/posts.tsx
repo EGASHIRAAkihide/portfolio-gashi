681d48313defd0922e1bc9ac7f9b8cbea490ba0d
akihide.egashira <42921740+EGASHIRAAkihide@users.noreply.github.com>
2022-11-18 15:51:39 +0900

    (feat) [main] add new type on lib/posts

--- lib/posts.tsx
+++ lib/posts.tsx
@@ -7,26 +7,21 @@ import html from 'remark-html';
 const postsDirectory = path.join(process.cwd(), 'data/posts')
 
 export function getSortedPostsData() {
-  // Get file names under /posts
   const fileNames = fs.readdirSync(postsDirectory)
-  const allPostsData = fileNames.map(fileName => {
-    // Remove ".md" from file name to get id
+  const allPostsData: any = fileNames.map(fileName => {
     const id = fileName.replace(/\.md$/, '')
 
-    // Read markdown file as string
     const fullPath = path.join(postsDirectory, fileName)
     const fileContents = fs.readFileSync(fullPath, 'utf8')
 
-    // Use gray-matter to parse the post metadata section
     const matterResult = matter(fileContents)
 
-    // Combine the data with the id
     return {
       id,
       ...matterResult.data
     }
   })
-  return allPostsData.sort((a, b) => {
+  return allPostsData.sort((a: { date: number; }, b: { date: number; }) => {
     if (a.date < b.date) {
       return 1
     } else {
