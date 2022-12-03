09c79be6ed4d4744be91bb576be036a9b9e2bfc3
akihide.egashira <42921740+EGASHIRAAkihide@users.noreply.github.com>
2022-12-03 18:46:18 +0900

    (feat) [issue-013] download機能を追加、まだ細かい実装はせず

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
 import Layout from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
 import Head from 'next/head';
-import { Button, Grid } from '@nextui-org/react';
+import { Grid } from '@nextui-org/react';
 import Card from '../components/card';
 import { Form } from '../components/form';
-import { Alert } from '../components/alert';
-import STATUS_MESSAGE from '../data/statusMessage';
+import useDownloader from 'react-use-downloader';
 
 type Props = {
   allPostsData: {
@@ -16,11 +15,32 @@ type Props = {
 }
 
 export default function Home({ allPostsData }: Props) {
+  const { size, elapsed, percentage, download, cancel, error, isInProgress } = useDownloader();
+  const fileurl = 'https://upload.wikimedia.org/wikipedia/commons/4/4d/%D0%93%D0%BE%D0%B2%D0%B5%D1%80%D0%BB%D0%B0_%D1%96_%D0%9F%D0%B5%D1%82%D1%80%D0%BE%D1%81_%D0%B2_%D0%BF%D1%80%D0%BE%D0%BC%D1%96%D0%BD%D1%8F%D1%85_%D0%B2%D1%80%D0%B0%D0%BD%D1%96%D1%88%D0%BD%D1%8C%D0%BE%D0%B3%D0%BE_%D1%81%D0%BE%D0%BD%D1%86%D1%8F.jpg'
   return (
     <Layout>
       <Head>
         <title>GASHI website</title>
       </Head>
+      <section style={{margin: "0 0 60px"}}>
+        <h2>About</h2>
+        <p>todo: portfolio</p>
+        <ul>
+          <li>size: {size}</li>
+          <li>isInProgress: {isInProgress ? 'true' : 'false'}</li>
+          <li>percentage: {percentage}</li>
+          <li>elapsed: {elapsed}</li>
+          <li>
+            <button onClick={() => download(fileurl, 'file')}>button</button>
+          </li>
+          <li>
+            <button onClick={() => cancel()}>cancel</button>
+          </li>
+          <li>
+            error: {JSON.stringify(error)}
+          </li>
+        </ul>
+      </section>
       <section style={{margin: "0 0 60px"}}>
         <h2>Blog</h2>
         <Grid.Container gap={2}>
