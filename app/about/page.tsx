853815b8d98f31cb6589ea9aa48dc2757ad9e5bd
akihide.egashira <42921740+EGASHIRAAkihide@users.noreply.github.com>
2023-03-05 11:56:29 -0500

    teat: lint-steged 3

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,10 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Page() {
-  return (
-    <div>
-      about
-      <Link href='/#'>go back to home</Link>
-    </div>
-  )
+	return (
+		<div>
+			about
+			<Link href='/#'>go back to home</Link>
+		</div>
+	);
 }
