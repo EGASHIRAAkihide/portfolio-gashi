853d7aa45ccab4fb26a283c0a4eebb36e82b135c
akihide.egashira <42921740+EGASHIRAAkihide@users.noreply.github.com>
2023-03-05 11:40:18 -0500

    feat: husky削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
-import Link from "next/link";
+import Link from "next/link"
 
 export default function Page() {
   return (
     <div>
-      <Link href='/#'>home</Link>
-      <Link href='/about'>about</Link>
+  <Link href='/#'>home</Link>
+  <Link href='/about'>about</Link>
+  <div>
+    image
+    <div>adsfasdf</div>
+  </div>
     </div>
   )
 }
