9a5b317e8af86eae4b6eac2506d148feab773f7f
akihide.egashira <42921740+EGASHIRAAkihide@users.noreply.github.com>
2023-03-05 12:13:57 -0500

    feat: sns情報追加

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ export default function Page() {
 			<Link href='/#'>home</Link>
 			<Link href='/about'>about</Link>
 			<div>
-				image
-				<div>adsfasdf</div>
+				<a href='https://github.com/EGASHIRAAkihide' target='_blank' rel="noopener noreferrer">GitHub</a>
+				<a href='https://www.linkedin.com/in/akihide-egashira-107070224/' target='_blank' rel="noopener noreferrer">LinkedIn</a>
+				<a href='https://www.instagram.com/gashi_japan/' target='_blank' rel="noopener noreferrer">Instagram</a>
 			</div>
 		</div>
 	);
